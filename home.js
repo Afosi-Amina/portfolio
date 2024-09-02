bf3c4697e78b1b6f7d138962af84e64c5d5a84a4
Afosi-Amina <136847293+Afosi-Amina@users.noreply.github.com>
2024-09-02 15:55:00 -0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,36 @@
+
+/* let b =document.getElementsByClassName("nav")
+function change(){
+                    
+b.style.fontSize="40px"
+console.log(a)
+
+
+                 
+}
+let a= Document.getElementsByTagName("a")
+a.addEventListener("click",change)  */
+/* let light = true;
+let btn = document.querySelector("a");
+let body = document.querySelector("a");
+function () {
+                    if (light) {
+                                        a.style.backgroundColor = "black";
+                                        a.style.color = "linearGradient(blue,red,yellow)";
+                                        light = false;
+                    } else {
+                                        a.style.backgroundColor = "white";
+                                        a.style.color = "black";
+                                        light = true;
+                    }
+}
+
+btn.addEventListener("mouseLeave", toggle); */
+
+
+/* const element = document.getElementsByTagName("a");
+
+element.addEventListener("mouseleave", function() {
+    console.log("Mouse has left the element");
+    // Additional actions can be added here
+}); */
